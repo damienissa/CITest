7571d8d093f6117e15d322f81906166185f5615f
Dmitry Virych <dmitry.virych@requestum.com>
2018-12-16 22:29:11 +0200

    project created

---
+++ CITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CITest
+//
+//  Created by Dima Virych on 12/16/18.
+//  Copyright © 2018 Virych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
